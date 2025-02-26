6c54ed63ea463b264ce7dda03e63df8874dd5fab
Vlad O <vladimir.orelov@cubbystorage.com>
2025-02-26 14:50:07 +0200

    feat: 🎸 prod - serve frontend from backend as static content

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'node:path';
+
+const imports =
+  process.env.NODE_ENV === 'production'
+    ? [
+      ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '.', 'app'),
+        exclude: ['/api*'],
+      }),
+    ]
+    : [];
 
 @Module({
-  imports: [],
+  imports,
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
