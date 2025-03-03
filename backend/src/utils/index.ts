dd9cf601fa3bc77729c6289c782f77eae34de7a4
Vlad O <vladimir.orelov@cubbystorage.com>
2025-03-03 17:28:53 +0200

    feat: 🎸 WIP: implement logic for longer periods

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,3 +1,13 @@
+import {
+  addSeconds,
+  eachDayOfInterval,
+  format,
+  isAfter,
+  isBefore,
+  isSameDay,
+} from 'date-fns';
+import { inMemoryPricePointsMock, inMemoryTradeInfoMock } from './mock';
+
 export type PricePoint = {
   timestamp: string;
   price: number;
@@ -14,6 +24,46 @@ export type TradeInfo = {
   minTime: string;
 };
 
+export const loadTradeInfo = (start: Date, end: Date) => {
+  const dates = eachDayOfInterval({
+    start,
+    end,
+  });
+
+  const tradeInfos: TradeInfo[] = [];
+  for (const date of dates) {
+    if (isSameDay(date, start) || isSameDay(date, end)) {
+      const pricePoints = loadPricePoints(date, start, end);
+      const tradeInfo = findMostProfitableTrade(pricePoints);
+      tradeInfos.push(tradeInfo);
+      continue;
+    }
+
+    const tradeInfo = loadSingleDateTradeInfo(date);
+    tradeInfos.push(tradeInfo);
+  }
+
+  return findMostProfitableTradeMultiDates(tradeInfos);
+};
+
+const loadSingleDateTradeInfo = (date: Date): TradeInfo => {
+  const key = format(date, 'yyyy-MM-dd');
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+  return inMemoryTradeInfoMock[key]();
+};
+
+const loadPricePoints = (date: Date, start: Date, end: Date): PricePoint[] => {
+  const key = format(date, 'yyyy-MM-dd');
+  const pricePoints = inMemoryPricePointsMock[key];
+
+  return pricePoints.filter(
+    (p) =>
+      isAfter(addSeconds(new Date(p.timestamp), 1), start) &&
+      isBefore(new Date(p.timestamp), addSeconds(end, 1)),
+  );
+};
+
 export const toTradeInfoArray = (pricePoints: PricePoint[][]) => {
   return pricePoints.map(findMostProfitableTrade);
 };
@@ -23,7 +73,7 @@ export const findMostProfitableTradeMultiDates = (info: TradeInfo[]) => {
 
   for (const element of info) {
     // max is before min
-    // so we ignore profitable trades within a day
+    // so the logic don't accidentally consider profitable trades from min/max
     arr.push({
       timestamp: element.maxTime,
       price: element.maxPrice,
@@ -50,9 +100,11 @@ export const findMostProfitableTradeMultiDates = (info: TradeInfo[]) => {
 
 // TODO: fix earliest and shortest requirement
 export const findMostProfitableTrade = (prices: PricePoint[]): TradeInfo => {
-  if (prices.length < 2) {
-    throw Error(`Not enough data! Prices length ${prices.length}`);
-  }
+  // TODO: does it make sense to buy and sell within a second
+  // I think yes!
+  // if (prices.length < 2) {
+  //   throw Error(`Not enough data! Prices length ${prices.length}`);
+  // }
 
   let minPrice = prices[0].price;
   let maxPrice = prices[0].price;
