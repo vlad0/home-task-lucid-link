d8459c52e5145be7ee0b2fe60a3a7227bde608eb
Vlad O <vladimir.orelov@cubbystorage.com>
2025-03-09 13:45:26 +0200

    feat: 🎸 add GenericException filter

---
+++ backend/src/generic-exception.filter.ts
@@ -0,0 +1,49 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  Logger,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
+
+@Catch()
+export class GenericExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(GenericExceptionFilter.name);
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request: Request = ctx.getRequest();
+
+    let status = 500;
+    let message: string | object = 'Ooops something went wrong';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      message = exception.getResponse();
+    } else if (exception instanceof Error) {
+      message = exception.message || message;
+    }
+
+    this.logger.error('Something went wrong:', {
+      exception,
+      path: request.url,
+
+      requestId: request.headers['x-request-id'],
+    });
+
+    let payload = {};
+    if (typeof message === 'string') {
+      payload['message'] = message;
+    } else {
+      payload = message;
+    }
+
+    response.status(status).json({
+      statusCode: status,
+
+      ...payload,
+    });
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GenericExceptionFilter } from './generic-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new GenericExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
